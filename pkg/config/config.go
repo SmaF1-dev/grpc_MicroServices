adb2a25c2f410ea3a92710535f1ae19e4a80fa87
SmaF1-dev <Nikita_Lyutyi@mail.ru>
2026-02-16 21:08:14 +0300

    [feat]: Common utils have been added.

---
+++ pkg/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func GetEnvAsInt(key string, defaultValue int) int {
+	if value, exists := os.LookupEnv(key); exists {
+		if intVal, err := strconv.Atoi(value); err == nil {
+			return intVal
+		}
+	}
+	return defaultValue
+}
---
+++ pkg/logger/logger.go
@@ -0,0 +1,23 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	infoLogger  = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+)
+
+func Info(format string, v ...interface{}) {
+	infoLogger.Printf(format, v...)
+}
+
+func Error(format string, v ...interface{}) {
+	errorLogger.Printf(format, v...)
+}
+
+func Fatal(format string, v ...interface{}) {
+	errorLogger.Fatalf(format, v...)
+}
