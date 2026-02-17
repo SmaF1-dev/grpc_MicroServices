d6cec96b5049c8fd92b7c4b6119bc0247c688354
SmaF1-dev <Nikita_Lyutyi@mail.ru>
2026-02-17 21:22:28 +0300

    [feat]: Connection to database and migration have been added.

---
+++ internal/repository/db.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func NewDB(host, port, user, password, dbname, sslmode string) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS orders (
+			id TEXT PRIMARY KEY,
+			user_id TEXT NOT NULL,
+			items JSONB NOT NULL,
+			total_amount DECIMAL(10, 2) NOT NULL,
+			payment_method TEXT NOT NULL,
+			status TEXT NOT NULL,
+			transaction_id TEXT,
+            created_at TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP NOT NULL
+			);
+		`
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create table: %w", err)
+	}
+
+	return db, nil
+}
